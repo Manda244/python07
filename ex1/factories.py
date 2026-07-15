21dd33b3ee4522f347ed61351f5bf0a0d6465066
marasolo <marasolo@student.42antananarivo.mg>
2026-07-15 07:12:57 +0300

    fin

--- ex1/factories.py
+++ ex1/factories.py
@@ -2,16 +2,16 @@
 # ########################################################################### #
 #   shebang: 1                                                                #
 #                                                          :::      ::::::::  #
-#   creatureFactory.py                                   :+:      :+:    :+:  #
+#   factories.py                                         :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
 #   By: marasolo <marasolo@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/07/14 20:12:28 by marasolo            #+#    #+#            #
-#   Updated: 2026/07/14 21:16:23 by marasolo           ###   ########.fr      #
+#   Updated: 2026/07/15 07:12:08 by marasolo           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
-from ex0.factories import CreatureFactory
+from ex0 import CreatureFactory
 from ex1.creatures import Sproutling, Bloomelle, Shiftling, Morphagon
 
 
